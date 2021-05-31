07a40569b920f4b7c74fb28b380ebad06978f957
Kamran Rizwan <70561110+Kamran-Rizwan0925@users.noreply.github.com>
2021-05-31 15:05:40 +0500

    Add files via upload

---
+++ PostsApp/mean-course/src/app/angular.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from "@angular/core";
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule,
+  MatPaginatorModule
+} from "@angular/material";
+import {MatDialogModule} from '@angular/material/dialog';
+
+@NgModule({
+  exports: [
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDialogModule
+  ]
+})
+
+export class AngularMaterialModule {
+
+}
